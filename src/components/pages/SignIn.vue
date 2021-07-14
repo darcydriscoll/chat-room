f4430f64b7bb8623a366654d11f2a0ca8544fbd5
Darcy Driscoll <darcy.driscoll@outlook.com>
2021-07-14 15:26:40 +1000

    [5] Change form action in SignIn.vue for back-end testing

--- src/components/pages/SignIn.vue
+++ src/components/pages/SignIn.vue
@@ -1,9 +1,9 @@
 <template>
   <BaseLayout>
-    <form action="index.html" method="post">
+    <form action="api/sign_in.php" method="post">
       <h2>Enter a nickname to start chatting</h2>
       <label for="username">Enter your desired nickname:</label>
-      <input type="text" name="username" id="username" required autocomplete="nickname">
+      <input type="text" name="nickname" id="username" required autocomplete="nickname">
       <input type="submit" name="submit" value="Enter">
       <!-- Accessible form errors -->
       <ul role="alert" aria-relevant="all"></ul>
