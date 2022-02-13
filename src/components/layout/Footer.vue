02867da9009990f9bcdf0cc0f489255d0ac620e7
darcy.driscoll <darcy.driscoll@outlook.com>
2022-02-13 20:38:13 +1100

    [5] Change name in footer

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="p-2 opacity-50 hover:opacity-100 select-none">
-    <p class="font-chat-body text-sm xl:text-md">Made with 🍩 by <span><a href="https://github.com/snarlinger/chat-room" class="underline text-green-700">a very good boy</a></span></p>
+    <p class="font-chat-body text-sm xl:text-md">Made with 🍩 by <span><a href="https://github.com/darcydriscoll/chat-room" class="underline text-green-700">Darcy D.</a></span></p>
   </footer>
 </template>
 
