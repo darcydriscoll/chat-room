ed22ec3a03a776008d1953673cacd98bbe992872
darcy.driscoll <darcy.driscoll@outlook.com>
2022-03-01 19:57:45 +1100

    [#28] Attempt to get Tailwind 3.0 but fail...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   purge: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
       fontFamily: {
